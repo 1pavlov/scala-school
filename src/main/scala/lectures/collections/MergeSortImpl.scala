fa462dbb2a3092d041a90185d430637700fcf959
1pavlov <vladimir@1pavlov.com>
2017-04-02 22:25:11 +0300

    finish MergeSortImpl

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -1,5 +1,7 @@
 package lectures.collections
 
+import scala.collection.JavaConverters._
+
 /**
   * Постарайтесь не использовать мутабильные коллекции и var
   * Подробнее о сортировке можно подсмотреть здесь - https://en.wikipedia.org/wiki/Merge_sort
@@ -8,6 +10,23 @@ package lectures.collections
   */
 object MergeSortImpl extends App {
 
-  def mergeSort(data: Seq[Int]): Seq[Int] = data
+  def mergeSort(data: Seq[Int]): Seq[Int] = {
+    val length = data.length
+    if (length <= 1)
+      data
+    else {
+      val (left, right) = data.splitAt(length/2)
+      merge(mergeSort(left), mergeSort(right))
+    }
+  }
 
+  def merge(left: Seq[Int], right: Seq[Int]): Seq[Int] = (left, right) match {
+    case(left, Nil) => left
+    case(Nil, right) => right
+    case(lHead :: lTail, rHead :: rTail) =>
+      if (lHead < rHead) Seq(lHead) ++ merge(lTail, right)
+      else Seq(rHead) ++ merge(left, rTail)
+  }
+
+  print(mergeSort(List(100, 70, 200, -200, -1, 1000)))
 }
