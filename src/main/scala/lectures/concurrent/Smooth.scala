be448dfacacea80a0404121e41b49083a6792ac9
Vladimir Pavlov <vladimir@1pavlov.com>
2017-05-19 14:28:16 +0300

    [hw7] Add a solution for Smooth

--- src/main/scala/lectures/concurrent/Smooth.scala
+++ src/main/scala/lectures/concurrent/Smooth.scala
@@ -1,6 +1,9 @@
 package lectures.concurrent
 
-import scala.concurrent.Future
+import java.util.concurrent.atomic.AtomicBoolean
+
+import scala.concurrent.{Future, Promise}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Smooth - это своебразный функциональный кэш, предназначенный для исключения повторных вызовов кода
@@ -16,9 +19,50 @@ import scala.concurrent.Future
   *
   */
 object Smooth{
-  //def apply(thunk: =>): Smooth = ???
+   def apply[T](thunk: => T): Smooth[T] = new Smooth[T](thunk)
 }
 
-class Smooth {
-   def apply(): Future[_] = ???
+// Implementation by SkayLarsen (Евгений Карасев)
+class Smooth[T](thunk: => T) {
+   @volatile private var result: Option[Future[T]] = None
+
+   def apply(): Future[T] = {
+      result.getOrElse(initSmooth)
+   }
+
+   private def initSmooth: Future[T] = {
+      synchronized {
+         result.getOrElse {
+            val f = Future(thunk)
+            result = Some(f)
+
+            f.onComplete { _ =>
+               result = None // Reset state, prepare for next task
+            }
+
+            f
+         }
+      }
+   }
+}
+
+// Implementation by pakondratyuk (Павел Кондратюк)
+class AnotherSmooth[T](thunk: => T) {
+
+   @volatile private var res = Promise[T]()
+
+   private val is_running = new AtomicBoolean(false)
+
+   def apply(): Future[T] = {
+      if (is_running.getAndSet(true)) {
+         res.future
+      } else {
+         res = Promise[T]()
+         val f = Future(thunk)
+         f onComplete { _ =>
+            is_running.set(false)
+         }
+         res.tryCompleteWith(f).future
+      }
+   }
 }
